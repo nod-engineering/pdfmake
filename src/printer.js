33a6b11e5786ae68a2948505e95341d9317c459f
mouthzipper <ramos.jeromeh@gmail.com>
2021-05-28 16:09:42 +0800

    Make image always on top

--- src/printer.js
+++ src/printer.js
@@ -177,6 +177,7 @@ function hasSection(items) {
 function sortByKey(array, key) {
 	return array.sort(function (a, b) {
 		var x = a[key]; var y = b[key];
+		if (x.type === 'image') return -1;
 		return ((x > y) ? -1 : ((x < y) ? 1 : 0));
 	});
 }
